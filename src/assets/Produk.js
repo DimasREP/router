68758765788e8952f2c1ba77063b15417e362993
kidiim <21221753112@smkassalaambandung.sch.id>
2023-07-31 13:57:05 +0700

    router

---
+++ src/assets/Produk.js
@@ -0,0 +1,24 @@
+export const produk = {
+    "produk" : [
+        {
+            "id": 1,
+            "nama" : "Handphone",
+            "harga" : "Rp.1.0000.000,00"
+        },
+        {
+            "id": 2,
+            "nama" : "Laptop",
+            "harga" : "Rp.5.0000.000,00"
+        },
+        {
+            "id": 3,
+            "nama" : "Laptop",
+            "harga" : "Rp.5.0000.000,00"   
+        },
+        {
+            "id": 4,
+            "nama" : "Sepatu",
+            "harga" : "Rp.2.000.000,00"
+       }
+    ]
+}
