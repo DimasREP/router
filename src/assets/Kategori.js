02c1f13a351b542e7b02d6512802d5ab9ac71955
kidiim <21221753112@smkassalaambandung.sch.id>
2023-08-02 11:11:41 +0700

    push

---
+++ src/assets/Kategori.js
@@ -0,0 +1,27 @@
+export const kategori = {
+    'kategori': [
+      {
+        'id': 1,
+        'nama': 'Elektronik',
+        'img': 'gambar1.jpeg'
+         
+      },
+      {
+        'id': 2,
+        'nama': 'Otomotif',
+        'img' : ''
+      },
+      {
+        'id': 3,
+        'nama': 'Olahraga',
+      },
+      {
+        'id': 4,
+        'nama': 'Alat Masak',
+      },
+      {
+        'id': 5,
+        'nama': 'Produk Pria',
+      },
+    ],
+  };
